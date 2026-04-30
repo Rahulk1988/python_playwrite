a77e7f5330fdd701cbb5472368dafcf4b4a16a1d
Rahul <khichadirahul@gmail.com>
2026-04-30 23:39:43 +0530

    fixed merge conflict in pytest fixture

--- conftest.py
+++ conftest.py
@@ -1,19 +1,10 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
-<<<<<<< HEAD
-@pytest.fixture
-def page():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-=======
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=True)  # headless for Jenkins
         page = browser.new_page()
-        page.wait_for_timeout(3000)
->>>>>>> ac181d6523a29b752126a7aab8ace7c786c1081a
         yield page
         browser.close()
